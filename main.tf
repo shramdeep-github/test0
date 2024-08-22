fe5f7f08bbfd28b48dc805d1a789ed2957091c35
Shramdeep Asawale <shramdeep@outlook.com>
2024-08-22 21:18:02 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,6 +12,12 @@ provider "azurerm" {
   features {}
 }
 
+# Generate a random integer to create a globally unique name
+resource "random_integer" "ri" {
+  min = 10000
+  max = 99999
+}
+
 # Create the resource group
 resource "azurerm_resource_group" "rg" {
   name     = "test-resource-grp-1"
@@ -29,7 +35,7 @@ resource "azurerm_service_plan" "appserviceplan" {
 
 # Create the web app, pass in the App Service Plan ID
 resource "azurerm_linux_web_app" "webapp" {
-  name                  = "test-webapp-1"
+  name                  = "test-webapp-${random_integer.ri.result}"
   location              = azurerm_resource_group.rg.location
   resource_group_name   = azurerm_resource_group.rg.name
   service_plan_id       = azurerm_service_plan.appserviceplan.id
